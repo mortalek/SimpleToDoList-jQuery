f98589456f39273f6f7c823e8df59b7c656c6867
Paweł Łukaszewski <31510458+mortalek@users.noreply.github.com>
2017-10-17 20:10:31 +0200

    Simple ToDoList

---
+++ todo.js
@@ -0,0 +1,55 @@
+$(document).ready(function(e) {
+	$("#add-todo").button({
+		icons: {
+			primary: "ui-icon-circle-plus"
+		}
+	}).click(function(){
+		$('#new-todo').dialog('open');
+	});
+	$("#new-todo").dialog({
+		modal: true,
+		autoOpen: false,
+		buttons: {
+			"Dodaj zadanie": function() {
+				var taskName = $('#task').val();
+				if (taskName === '') {
+					return false;
+				}
+				var taskHTML = '<li><span class="done">%</span>';
+				taskHTML += '<span class="delete">x</span>';
+				taskHTML += '<span class="task"></span></li>';
+				var $newTask = $(taskHTML);
+				$newTask.find('.task').text(taskName);
+				$newTask.hide();
+				$('#todo-list').prepend($newTask);
+				$newTask.show('clip',250).effect('highlight',1000);
+				$(this).dialog('close');
+			},
+			"Anuluj": function() {
+				$(this).dialog('close');
+		}
+	},
+	close: function() {
+		$('#new-todo input').val(''); 
+	}	
+});
+	$("#todo-list").on('click', '.done', function() {
+		var $taskItem = $(this).parent('li');
+		$taskItem.slideUp(250, function() {
+			var $this = $(this);
+			$this.detach();
+			$('#completed-list').prepend($this);
+			$this.slideDown();
+		});
+	});
+	$('.sortlist').sortable({
+		connectWith: '.sortlist',
+		placeholder: 'ui-state-highlight',
+		cancel: '.delete, .done'
+	});
+	$('.sortlist').on('click', '.delete', function() {
+		$(this).parent('li').effect('puff', function() {
+			$(this).remove();
+		});
+	});
+});
